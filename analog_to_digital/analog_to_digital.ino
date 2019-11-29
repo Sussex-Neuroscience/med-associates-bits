8c92268fe3b024e2655868470e5cec641d5726ea
Andre Maia Chagas <andremaia.chagas@gmail.com>
2019-11-29 10:08:01 +0000

    Update analog_to_digital.ino
    so that two led indicators are reporting the animal's interaction with the device.

--- analog_to_digital/analog_to_digital.ino
+++ analog_to_digital/analog_to_digital.ino
@@ -2,8 +2,9 @@ int IRrecPin = A0;
 int IRrecPin1 = A2;
 int comparatorPin = A1;
 int digOutPin = 13;
-int ledInd = 12;
-int digOutPin1 = 3;
+int digOutPin1 = 12;
+int ledInd = 2;
+int ledInd1= 3;
 int IRread = 0;
 int compRead = 0;
 int IRread1 = 0;
@@ -13,8 +14,10 @@ int compRead1 = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(digOutPin, OUTPUT);
-  pinMode(ledInd, OUTPUT); 
   pinMode(digOutPin1, OUTPUT);
+  pinMode(ledInd, OUTPUT); 
+  pinMode(ledInd1, OUTPUT); 
+  
   // put your setup code here, to run once:
 
 }
@@ -41,10 +44,11 @@ void loop() {
 
   if (IRread1>=512){
     digitalWrite(digOutPin1, LOW);
-    
+    digitalWrite(ledInd1, LOW);
     }
   else{
     digitalWrite(digOutPin1, HIGH);
+    digitalWrite(ledInd1, HIGH);
   }
   
   // put your main code here, to run repeatedly:
